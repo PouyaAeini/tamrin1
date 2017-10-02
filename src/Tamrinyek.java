ca307301f8468bc5e960a048e6a7800e738ce4cb
aeini <p.aeini2571@outlook.com>
2017-10-02 15:43:44 +0330

    First commit

---
+++ src/Tamrinyek.java
@@ -0,0 +1,171 @@
+/*
+
+*/
+
+import com.sun.xml.internal.ws.commons.xmlutil.Converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by PouyA on 9/26/2017.
+ */
+
+public class Tamrinyek {
+
+    public int d;
+
+    public static void main(String[] args) {
+//tamrine yek
+/////////////////////////////////////////
+
+        //  Fib();
+
+//////////////////////////////////////////
+
+//        long N = 1;
+//
+//        for (int i = 1; ; i++) {
+//            N = fibonacci(i);
+//            if(N<1000)
+//                System.out.println(N);
+//            else
+//                break;
+//
+//    }
+//////////////////////////////////////////////tamrin2
+//        Scanner user_input = new Scanner( System.in );
+//        String num;
+//        System.out.print("Enter your Number: ");
+//        num = user_input.next( );
+//        if(palindrom(num))
+//            System.out.print("is palindrom ");
+//        else
+//            System.out.print("is not palindrom ");
+        /////////////////////////////////////////tamrin3
+//        if(power(64))
+//            System.out.print("ok");
+//        else
+//            System.out.print("no");
+        /////////////////////////////////////tamrin4
+//        chap();
+/////////////////////////////////////////////tamrin5
+
+//        System.out.print(masahat(4));
+
+        ///////////////////////////tamrin6
+//       star(9);
+        ////////////////////////////////tamrin7
+
+//        System.out.println(fac(4));
+//        System.out.println(factorial(4));
+
+
+    }
+
+    public static void Fib() {
+
+
+        long f = 0, g = 1;
+
+        for (int i = 1; f < 1000; i++) {
+
+            f = f + g;
+
+            g = f - g;
+
+            if (f < 1000)
+
+                System.out.println(f);
+        }
+
+
+    }
+
+
+    public static long fibonacci(int n) {
+        if (n <= 1) return n;
+
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+
+    public static boolean palindrom(String temp) {
+        String ch = new StringBuilder(temp).reverse().toString();
+        if (temp.compareTo(ch) == 0)
+            return true;
+        return false;
+    }
+
+    public static boolean power(int temp) {
+//        int temp1 = Integer.parseInt(temp);
+//        int temp2 = 1;
+//        while (temp1>temp2)
+//        {
+//            temp1 = Integer.parseInt(temp) * temp1;
+//        }
+        if ((temp & (temp - 1)) == 0)
+            return true;
+        return false;
+    }
+
+
+    public static void chap() {
+        char ch = 'a';
+        int ich = ch;
+        for (int i = 0; i < 13; i++) {
+            System.out.println((char) ich);
+            ich = ich + 2;
+
+        }
+
+    }
+
+    public static double masahat(double r) {
+        return (3.14 * (r * r));
+    }
+
+    public static void star(int num) {
+        for (int i = 1; i < num * 2; i = i + 2) {
+            if (i <= num)
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+            else
+                for (int r = 1; r < (num - (i - num))-1; r++)
+                    System.out.print("*");
+            System.out.println("");
+        }
+
+    }
+
+
+    public static double fac(double num) {
+        long result = 1;
+
+        while (num>0) {
+
+            result *= num;
+
+            num--;
+
+        }
+
+        return result;
+
+    }
+
+    public static long factorial(int n) {
+
+        if (n == 0)
+
+            return 1;
+
+        else
+
+            return n * factorial(n - 1);
+
+    }
+}
+
